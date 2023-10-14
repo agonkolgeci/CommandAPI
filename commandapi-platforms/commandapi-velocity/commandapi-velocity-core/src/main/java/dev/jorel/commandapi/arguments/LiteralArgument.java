078422087a4136b4dccef66d4389c5f827c0d73c
willkroboth <46540330+willkroboth@users.noreply.github.com>
2023-10-14 15:34:39 -0400

    Address some SonarCloud code smells
    Whoops, these actually are some silly mistakes I missed :P, thanks SonarCloud

--- commandapi-platforms/commandapi-velocity/commandapi-velocity-core/src/main/java/dev/jorel/commandapi/arguments/LiteralArgument.java
+++ commandapi-platforms/commandapi-velocity/commandapi-velocity-core/src/main/java/dev/jorel/commandapi/arguments/LiteralArgument.java
@@ -145,7 +145,7 @@ public class LiteralArgument extends Argument<String> implements Literal<Argumen
 
 	@Override
 	public void checkPreconditions(List<Argument<?>> previousArguments, List<String> previousNonLiteralArgumentNames) {
-		super.checkPreconditions(previousArguments, previousNonLiteralArgumentNames);
+		Literal.super.checkPreconditions(previousArguments, previousNonLiteralArgumentNames);
 	}
 
 	@Override
